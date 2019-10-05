bc2d4aba81b16ab550a3d1d845d22bc1540f952b
Jianheng Huang <vincentyaha@hotmail.com>
2019-10-05 17:58:06 +1000

    remove directory slider

--- material-slider/main.js
+++
@@ -1,4 +0,0 @@
-import {MDCSlider} from '/@material/slider';
-
-const slider = new MDCSlider(document.querySelector('.mdc-slider'));
-slider.listen('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
